3f4d7eaf9ab5cdf50070deea3d0f98e5e9f2b605
Vaibhav <vhake45@gmail.com>
2026-05-28 23:43:02 +0530

    Hallow Triangle

---
+++ Pattern/HPattern38.java
@@ -0,0 +1,15 @@
+class HPattern38{
+	public static void main(String[]args){
+		int n=4;
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				if(j==1||i==1||i+j==n+1){
+					System.out.print("* ");
+				}else{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
