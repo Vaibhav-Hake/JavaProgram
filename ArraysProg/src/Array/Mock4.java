871c9e4b9eb76430fbb5fd94ccb7da4607bcf946
Vaibhav <vhake45@gmail.com>
2026-07-07 20:06:30 +0530

    Mock freq Program

---
+++ ArraysProg/src/Array/Mock4.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class Mock4 {
+	public static void main(String[]args) {
+		String a="Java is easy Java";
+		String []b= a.split(" ");
+		freq(b);
+	}
+	public static void freq(String[]a) {
+		boolean[]v=new boolean[a.length];
+		for(int i=0;i<a.length;i++) {
+			if(v[i]) {
+				continue;
+			}
+			int ct=1;
+			for(int j=i+1;j<a.length;j++) {
+				if(a[i].equals(a[j])) {
+					ct++;
+					v[j]=true;
+				}
+			}
+			System.out.println(a[i]+"-->"+ct);
+		}
+		
+	}
+
+}
