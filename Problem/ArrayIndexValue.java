5886edca254ba6230a5862d6b10afbc9d80ab44e
Vaibhav <vhake45@gmail.com>
2026-07-20 22:56:09 +0530

    Problem

---
+++ Problem/ArrayIndexValue.java
@@ -0,0 +1,10 @@
+class ArrayIndexValue {
+    public static int findElementAtIndex(int i, int[] arr) {
+        // code here
+        return arr[i];
+    }
+	public static void main(String[]args){
+		int[]a={10,20,30,40};
+		System.out.println(findElementAtIndex(2,a));
+	}
+}
