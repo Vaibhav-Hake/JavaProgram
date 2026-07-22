c2db09185af9df59fd26dc170cc26c729b09f44e
Vaibhav <vhake45@gmail.com>
2026-07-22 23:10:30 +0530

    Sum Digit

---
+++ Problem/SumDigit.java
@@ -0,0 +1,15 @@
+class SumDigit {
+   public  static int sumOfDigits(int n) {
+        // code here
+        int sum=0;
+        while(n!=0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+	public static void main(String[]args){
+		
+		System.out.println(sumOfDigits(256));
+	}
+}
