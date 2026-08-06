9543ecde925d8f57674e047ab6994275ae0ccbf6
Vaibhav <vhake45@gmail.com>
2026-08-07 00:01:09 +0530

    Leetcode 1678

--- Problem/LeetCode1678.java
+++ Problem/LeetCode1678.java
@@ -1,17 +1,21 @@
 class LeetCode1678 {
     public static  String interpret(String command) {
-         String res="";
+         //String res="";
+		  StringBuilder res=new StringBuilder();
          for(int i=0;i<command.length();i++){
             if(command.charAt(i)=='G'){
-                res+='G';
+               // res+='G';
+				  res.append('G');
             }else if(command.charAt(i)=='('&&command.charAt(i+1)==')'){
-                res+='o';
+               // res+='o';
+				  res.append('o');
             }
             else if(command.charAt(i)=='('&&command.charAt(i+1)=='a'){
-                res+="al";
+              //  res+="al";
+				  res.append("al");
             }
          }
-         return res;
+         return new String(res);
     }
 	public static void main(String[]args){
 		System.out.println(interpret("G()()(al)"));
