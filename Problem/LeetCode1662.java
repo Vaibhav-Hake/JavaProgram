8da1e0eea0d71ed669e8f517da284a4e47fbd957
Vaibhav <vhake45@gmail.com>
2026-08-07 00:39:03 +0530

    LeetCode 1662

---
+++ Problem/LeetCode1662.java
@@ -0,0 +1,18 @@
+class LeetCode1662 {
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String s1="";
+        String s2="";
+        for(int i=0;i<word1.length;i++){
+            s1+=word1[i];
+        }
+         for(int i=0;i<word2.length;i++){
+            s2+=word2[i];
+        }
+        return s1.equals(s2);
+    }
+		public static void main(String[]args){
+			String[] word1 = {"ab", "c"};
+		String[]word2 = {"a", "bc"};
+		System.out.println( arrayStringsAreEqual(word1,word2));
+	}
+}
