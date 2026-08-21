29547dd4adecf6000997b245a5945af35ed011e4
Vaibhav <vhake45@gmail.com>
2026-08-21 11:12:45 +0530

    Selenium

---
+++ Selenium/Com.crm.SeleniumM4/src/Basic/MultipleElement.java
@@ -0,0 +1,29 @@
+package Basic;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MultipleElement {
+public static void main(String[] args) throws InterruptedException {
+	ChromeDriver d=new ChromeDriver();
+	d.manage().window().maximize();
+    d.get("https://demowebshop.tricentis.com/");
+	
+	Thread.sleep(2000);
+	//first way : common atrribute
+//	List<WebElement> polls = d.findElements(By.name("pollanswers-1"));
+//	for (WebElement poll : polls) {
+//		poll.click();
+//		Thread.sleep(2000);
+//	}
+	//2nd way: traversing common parent to child element
+	List<WebElement> polls = d.findElements(By.xpath("//ul[@class='poll-options']/li/input"));
+	for (WebElement poll : polls) {
+		poll.click();
+		Thread.sleep(2000);
+	}
+}
+}
