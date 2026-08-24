94dd94399df58c67e40a87fc59bda7e9f59ce940
Vaibhav <vhake45@gmail.com>
2026-08-24 12:31:16 +0530

    selenium

--- Selenium/Com.crm.SeleniumM4/src/MouseAndKeyBoard/Scrolling.java
+++ Selenium/Com.crm.SeleniumM4/src/MouseAndKeyBoard/Scrolling.java
@@ -1,6 +1,8 @@
 package MouseAndKeyBoard;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -12,9 +14,25 @@ public static void main(String[] args) throws InterruptedException {
 	
 	Thread.sleep(2000);
 	Actions act=new Actions(d);
-	act.scrollByAmount(0, 800);
+//	//Using Acess
+//	//act.scrollByAmount(0, 800);
+//	//Scroll Down using element
+//	WebElement facebook = d.findElement(By.xpath("//a[text()='Facebook']"));
+//	Thread.sleep(2000);
+//	act.scrollToElement(facebook).perform();
+//	//d.findElement(By.xpath("//a[text()='Facebook']")).click();
+//	
+//	//Scroll up using element
+//		WebElement excelent = d.findElement(By.id("pollanswers-1"));
+//		Thread.sleep(2000);
+//		act.scrollToElement(excelent).perform();
+	
+	//Scrolling using KeyBoard 
+	act.keyDown(Keys.PAGE_DOWN).perform();
+	// if want to keys release then we use keysUp
+	act.keyUp(Keys.PAGE_DOWN).perform();
 	Thread.sleep(2000);
-	d.findElement(By.xpath("//a[text()='Facebook']")).click();
+	act.keyDown(Keys.PAGE_UP).perform();
 	Thread.sleep(2000);
 	d.quit();
 }
